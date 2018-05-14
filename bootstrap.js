803086bc6e582b764d637bfe8394dcae1c49f0ac
Erik Landvall <erik@landvall.se>
2018-05-14 11:10:35 +0200

    Lifted out the websocket server implementation to the js.core module ...to serve as an addon when needed

---
+++ bootstrap.js
@@ -0,0 +1,5 @@
+module.exports = (config_core) =>
+{
+  config_core.server.ws         = __dirname
+  config_core.server.websocket  = __dirname
+}
---
+++ test/integration.test.js
@@ -0,0 +1,28 @@
+describe('websocket integration tests', async () =>
+{
+  const
+  expect  = require('chai').expect,
+  context = require('mochawesome/addContext')
+
+  let server, client
+
+  beforeEach(() =>
+  {
+
+  })
+
+  afterEach(() =>
+  {
+
+  })
+
+  it('websocket responding', () =>
+  {
+
+  })
+
+  it('websocket responding with multiple responses', () =>
+  {
+
+  })
+})
